bb7d7b98a96d9bc7045ad1ad35332bd408dbb53c
Gaenco <00037021@uca.edu.sv>
2023-03-31 12:22:25 -0600

    Labo3

---
+++ ConstraintLayout/app/src/main/java/com/cortezjoya/constraintlayout/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.cortezjoya.constraintlayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var money:TextView
+    private lateinit var 
+
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
