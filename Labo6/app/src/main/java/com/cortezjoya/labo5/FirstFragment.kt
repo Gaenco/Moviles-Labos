75e6757133447d4f319dc281ba586af65df34f90
Gaenco <00037021@uca.edu.sv>
2023-04-28 11:15:36 -0600

    feat: Labo6

---
+++ Labo6/app/src/main/java/com/cortezjoya/labo5/FirstFragment.kt
@@ -0,0 +1,40 @@
+package com.cortezjoya.labo5
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.cardview.widget.CardView
+import androidx.navigation.findNavController
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class FirstFragment : Fragment() {
+
+    private lateinit var btnActionSend: FloatingActionButton
+    private lateinit var cardViewStarWars: CardView
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btnActionSend = view.findViewById(R.id.btnActionSend)
+        cardViewStarWars = view.findViewById(R.id.card_view_star_wars)
+
+        btnActionSend.setOnClickListener{
+            it.findNavController().navigate(R.id.action_firstFragment_to_secondFragment)
+        }
+
+        cardViewStarWars.setOnClickListener{
+            it.findNavController().navigate(R.id.action_firstFragment_to_thirdFragment)
+        }
+    }
+
+}
