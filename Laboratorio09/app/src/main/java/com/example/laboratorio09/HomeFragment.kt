d0db2f7f9f5f36f8a8cad11b607b9613b86e210d
Gaenco <00037021@uca.edu.sv>
2023-05-19 12:09:16 -0600

    Labo9

---
+++ Laboratorio09/app/src/main/java/com/example/laboratorio09/HomeFragment.kt
@@ -0,0 +1,43 @@
+package com.example.laboratorio09
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.findNavController
+import com.example.corutinesdemo.R
+import com.example.corutinesdemo.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    private lateinit var buttonMainThreadFragment: Button
+    private lateinit var buttonCoroutinesFragment: Button
+
+    private lateinit var binding: FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        buttonMainThreadFragment = view.findViewById(R.id.mainThreadFragment)
+        buttonCoroutinesFragment = view.findViewById(R.id.coroutinesFragment)
+
+        buttonMainThreadFragment.setOnClickListener{
+            it.findNavController().navigate(R.id.action_homeFragment_to_mainThreadFragment)
+        }
+
+        buttonCoroutinesFragment.setOnClickListener{
+            it.findNavController().navigate(R.id.action_homeFragment_to_coroutinesFragment)
+        }
+
+    }
+}
